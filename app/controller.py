e1b7565b39151f86d9e97a7671977dc79ff2f51b
sudoskys <coldlando@hotmail.com>
2024-03-17 03:21:10 +0800

    :art: refactor(controller): update content formatting in controller.py
    - **🥽 AnimeScore: {result.anime_score}**
    - 🔍 **Infer Tags**: `{result.anime_tags}`
    - ✏NovelAI Prompt: `{read_prompt}`
    - **Model: `{read_model.value}`**
    - Source: `{meta_data.Source}`
    - **🧊Not Signed by NovelAI**
    - ✏Mode: `{mode}`
    - 🌟 Characters: `{result.characters}`

--- app/controller.py
+++ app/controller.py
@@ -56,7 +56,7 @@ class BotRunner(object):
         result = await pipeline_tag(trace_id="test", content=file_data)
         content = [
             f"**🥽 AnimeScore: {result.anime_score}**",
-            f"🔍 **Infer Tags**: `{result.anime_tags}`",
+            f"**🔍 Infer Tags**: `{result.anime_tags}`",
         ]
         try:
             file_data.seek(0)
@@ -80,16 +80,16 @@ class BotRunner(object):
         except Exception as e:
             logger.exception(e)
         else:
-            content.append(f"✏**NovelAI Prompt:** `{read_prompt}`")
+            content.append(f"**✏ NovelAI Prompt:** `{read_prompt}`")
             if read_model:
-                content.append(f"**Model:** `{read_model.value}`")
+                content.append(f"**📦 Model:** `{read_model.value}`")
             if meta_data.Source:
-                content.append(f"**Source:** `{meta_data.Source}`")
+                content.append(f"**📦 Source:** `{meta_data.Source}`")
             if not is_novelai:
-                content.append("**🧊Not Signed by NovelAI**")
-            content.append(f"✏**Mode**: `{mode}`")
+                content.append("**🧊 Not Signed by NovelAI**")
+            content.append(f"**✏ Mode**: `{mode}`")
         if result.characters:
-            content.append(f"🌟 **Characters:** `{result.characters}`")
+            content.append(f"**🌟 Characters:** `{result.characters}`")
         prompt = telegramify_markdown.convert("\n".join(content))
         file_data.close()
         return prompt
