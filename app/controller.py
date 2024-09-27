d79f794b2e18a0822eb3ac2800640bbd05b8cba8
sudoskys <coldlando@hotmail.com>
2024-09-28 01:48:50 +0800

    ✨ feat(controller): add option to hide long text in tagger response
    Added `hidden_long_text` parameter to `tagger` method to control
    the display of long text responses. Updated relevant method calls
    to include the new parameter.

--- app/controller.py
+++ app/controller.py
@@ -73,12 +73,12 @@ class BotRunner(object):
                 mode += "+VibeTransfer"
         except Exception as e:
             logger.info(f"Empty metadata {e}")
-            content.append(f"```\n{result.anime_tags}```\n")
+            content.append(f"```{result.anime_tags}```")
         else:
             if hidden_long_text:
                 content.append(f"\n>{result.anime_tags}\n")
             else:
-                content.append(f"```\n{result.anime_tags}```\n")
+                content.append(f"```{result.anime_tags}```")
             if read_prompt:
                 content.append(f"**📦 Prompt:** `{read_prompt}`")
             if read_model:
